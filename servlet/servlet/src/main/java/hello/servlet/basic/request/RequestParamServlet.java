cd0e075b5c9f5e85a9ce4cd782902d7b274df09f
Son <rooneei@naver.com>
2023-03-27 23:52:53 +0900

    [add]

--- servlet/servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ servlet/servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -5,13 +5,16 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jdk.jfr.Frequency;
 
 import java.io.IOException;
 
-@WebServlet(name = "reqeustHeaderServlet",urlPatterns = "request-header")
-public class ReqeustHeaderServlet extends HttpServlet {
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+    // ctrl + o (override 인듯)
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.service(req, resp);
+        System.out.println("Request");
+
     }
 }
